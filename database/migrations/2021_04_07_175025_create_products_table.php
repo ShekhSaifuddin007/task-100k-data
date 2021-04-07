5f34f4931e86e22ea7da906faed8d1193cd4af16
ShekhSaifuddin007 <saifsabbir4@gmail.com>
2021-04-08 01:13:14 +0600

    table created

---
+++ database/migrations/2021_04_07_175025_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 150);
+            $table->foreignId('category_id')->constrained();
+            $table->foreignId('brand_id')->constrained();
+            $table->foreignId('group_id')->constrained();
+            $table->enum('type', [
+                'standard', 'variant'
+            ])->default('standard');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
